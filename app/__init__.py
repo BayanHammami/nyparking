22d6d889d90225e833002813961bc62ebb86fcc9
bor_123 <mail@nomail.com>
2014-10-13 23:43:18 +1100

    e

--- app/__init__.py
+++ app/__init__.py
@@ -5,10 +5,13 @@ from flask.ext.restful import reqparse
  
 app = Flask(__name__, static_url_path = "")
 
+
+#validation of querystring parameters
 parser = reqparse.RequestParser()
 parser.add_argument('year', type=int, required=True, location='args', help='year cannot be converted')
 parser.add_argument('radius', type=int, required=True, location='args', help='Radius cannot be converted')
 
+#error handlers
 @app.errorhandler(400)
 def not_found(error):
     return make_response(jsonify( { 'error': 'Bad request' } ), 400)
@@ -17,21 +20,7 @@ def not_found(error):
 def not_found(error):
     return make_response(jsonify( { 'error': 'Not found' } ), 404)
  
-tasks = [
-    {
-        'id': 1,
-        'title': u'Buy groceries',
-        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol', 
-        'done': False
-    },
-    {
-        'id': 2,
-        'title': u'Learn Python',
-        'description': u'Need to find a good Python tutorial on the web', 
-        'done': False
-    }
-]
-
+#base json response
 response = {
     'most_likely_probability': 0.3,
     'p_interval_start': 0.2,
@@ -55,64 +44,14 @@ response = {
     ]
 }
  
-def make_public_task(task):
-    new_task = {}
-    for field in task:
-        if field == 'id':
-            new_task['uri'] = url_for('get_task', task_id = task['id'], _external = True)
-        else:
-            new_task[field] = task[field]
-    return new_task
-    
-@app.route('/todo/api/v1.0/tasks', methods = ['GET'])
-def get_tasks():
+#handle get request    
+@app.route('/nyparking/api/v1.0/model', methods = ['GET'])
+def get_model():
     args = parser.parse_args()
     response['most_likely_probability']=args['radius']
     return jsonify( { 'tasks': response } )
  
-@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods = ['GET'])
-def get_task(task_id):
-    response['most_likely_probability']=task_id
-    return jsonify( { 'task': response } )
- 
-@app.route('/todo/api/v1.0/tasks', methods = ['POST'])
-def create_task():
-    if not request.json or not 'title' in request.json:
-        abort(400)
-    task = {
-        'id': tasks[-1]['id'] + 1,
-        'title': request.json['title'],
-        'description': request.json.get('description', ""),
-        'done': False
-    }
-    tasks.append(task)
-    return jsonify( { 'task': make_public_task(task) } ), 201
- 
-@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods = ['PUT'])
-def update_task(task_id):
-    task = filter(lambda t: t['id'] == task_id, tasks)
-    if len(task) == 0:
-        abort(404)
-    if not request.json:
-        abort(400)
-    if 'title' in request.json and type(request.json['title']) != unicode:
-        abort(400)
-    if 'description' in request.json and type(request.json['description']) is not unicode:
-        abort(400)
-    if 'done' in request.json and type(request.json['done']) is not bool:
-        abort(400)
-    task[0]['title'] = request.json.get('title', task[0]['title'])
-    task[0]['description'] = request.json.get('description', task[0]['description'])
-    task[0]['done'] = request.json.get('done', task[0]['done'])
-    return jsonify( { 'task': make_public_task(task[0]) } )
-    
-@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods = ['DELETE'])
-def delete_task(task_id):
-    task = filter(lambda t: t['id'] == task_id, tasks)
-    if len(task) == 0:
-        abort(404)
-    tasks.remove(task[0])
-    return jsonify( { 'result': True } )
+
     
 if __name__ == '__main__':
     app.run(debug = True)
