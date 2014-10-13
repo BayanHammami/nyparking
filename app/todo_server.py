e793988bc41bce1e778864a79e783a04fb770143
bor_123 <mail@nomail.com>
2014-10-13 20:34:09 +1100

    fix

--- app/todo_server.py
+++ app/todo_server.py
@@ -28,29 +28,6 @@ tasks = [
         'done': False
     }
 ]
-
-response = {
-    'most_likely_probability': 0.3,
-    'p_interval_start': 0.2,
-    'p_interval_end': 0.4,
-    'inputs': {
-        'year': 2013,
-        'radius': 100,
-        'latitude': 40.662670,
-        'longitude': -73.908203,
-        'start_time': '14:00',
-        'duration': 60
-    },
-    'number_of_fines': 80,
-    'number_of_periods': 365,
-    'number_of_periods_with_fines': 42, # does not match probability, but never mind!
-    'historical': [
-        { 'lat': 40.662670, 'long': -73.908203, 'date': '2013-03-02' },
-        { 'lat': 40.664764, 'long': -73.904900, 'date': '2013-05-03' },
-        { 'lat': 40.690067, 'long': -73.991173, 'date': '2013-06-06' },
-        { 'lat': 40.692677, 'long': -73.988159, 'date': '2013-07-01' }
-    ]
-}
  
 def make_public_task(task):
     new_task = {}
