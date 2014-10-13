c4bbb52ad202b695f630b7dfc78b3c916f45b181
bor_123 <mail@nomail.com>
2014-10-14 10:17:31 +1100

    e

---
+++ app/config.py
@@ -0,0 +1,22 @@
+# Statement for enabling the development environment
+DEBUG = True
+
+# Define the application directory
+import os
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))  
+
+# Define the database - we are working with
+# SQLite for this example
+SQLALCHEMY_DATABASE_URI = 'postgresql://user:password@demodata.c3ctvtncfc4b.ap-southeast-2.rds.amazonaws.com/demodata
+
+DATABASE_CONNECT_OPTIONS = {}
+
+# Enable protection agains *Cross-site Request Forgery (CSRF)*
+CSRF_ENABLED     = True
+
+# Use a secure, unique and absolutely secret key for
+# signing the data. 
+CSRF_SESSION_KEY = "secret"
+
+# Secret key for signing cookies
+SECRET_KEY = "secret"
