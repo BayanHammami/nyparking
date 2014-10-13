2adeb053466f46c77491df5c12378cc058758470
bor_123 <mail@nomail.com>
2014-10-13 16:24:31 +1100

    simple json

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,24 @@
 from flask import Flask
-from api import views
+
+
+@app.route("/")
+def hello():
+    return "I heart NY!"
+
+
+@app.route('/getheatmap', methods = ['GET'])
+def api_getheatmap():
+    data = {
+        'hello'  : 'world',
+        'number' : 3
+    }
+    js = json.dumps(data)
+
+    resp = Response(js, status=200, mimetype='application/json')
+    resp.headers['Link'] = 'http://luisrei.com'
+
+    return resp
+
 
 app = Flask(__name__)
 
