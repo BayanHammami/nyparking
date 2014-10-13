36a43f3a087f1db527221623370996563f974db1
bor_123 <mail@nomail.com>
2014-10-13 14:02:06 +1100

    mucking around

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,7 @@
 from flask import Flask
-app = Flask(__name__)
+from api import views
 
-@app.route("/")
-def hello():
-    return "I heart NY!"
+app = Flask(__name__)
 
 if __name__ == "__main__":
     app.run()
