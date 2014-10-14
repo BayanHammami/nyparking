8a72ec6f4a8df44f441c44478cbdfd75c58fa0f8
bor_123 <mail@nomail.com>
2014-10-14 20:59:55 +1100

    t

--- app/__init__.py
+++ app/__init__.py
@@ -67,7 +67,7 @@ def get_summary_pg():
     #result = db.session.query().from_statement('select * from app_summary_vw').all()
     connection = db.engine.connect()
     #result = connection.execute("select number_of_fines from public.app_summary_vw")
-    result = connection.execute("SELECT count(*) as out_var FROM pg_catalog.pg_tables")
+    result = connection.execute("SELECT CURRENT_DATE as out_var")
     for row in result:
         outv =  row['out_var']
     connection.close()
