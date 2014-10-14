1c26d352c49884935e7be262d1a067d4877579ef
bor_123 <mail@nomail.com>
2014-10-14 19:58:13 +1100

    r

--- app/__init__.py
+++ app/__init__.py
@@ -37,9 +37,9 @@ parser.add_argument('lng', type=str, required=True, location='args', help='long 
 def not_found(error):
     return make_response(jsonify( { 'error': 'Bad request' } ), 400)
  
-@app.errorhandler(404)
-def not_found(error):
-    return make_response(jsonify( { 'error': 'Not found' } ), 404)
+#@app.errorhandler(404)
+#def not_found(error):
+#    return make_response(jsonify( { 'error': 'Not found' } ), 404)
  
 #base json response
 response = {
