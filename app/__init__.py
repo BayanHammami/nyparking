dc0fc5fa0ceed9edf7221fc04b948abab3cde6b6
bor_123 <mail@nomail.com>
2014-10-14 14:43:21 +1100

    t

--- app/__init__.py
+++ app/__init__.py
@@ -64,8 +64,8 @@ response = {
 }
  
 def get_summary_pg():
-    result = db.session.query().from_statement('select app_model_summary_sp(:lat,:lng,:radius)').params(lat = 1, lng = 1,radius = 1).all()
-    #result = db.execute('SELECT app_model_summary_sp(?,?,?)', 1, 2, 3).fetchall()
+    #result = db.session.query().from_statement('select app_model_summary_sp(:lat,:lng,:radius)').params(lat = 1, lng = 1,radius = 1).all()
+    result = db.execute('SELECT app_model_summary_sp(?,?,?)', 1, 2, 3).fetchall()
     return result
 
 def run_model(args):
