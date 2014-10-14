466c1b21451e5ba1ab61cb63ca190cf9af40ba29
bor_123 <mail@nomail.com>
2014-10-14 23:35:51 +1100

    t

--- app/__init__.py
+++ app/__init__.py
@@ -52,7 +52,7 @@ response = {
 }
  
 def get_summary_pg():
-    result = db.session.query().from_statement('select number_of_fines from app_model_summary_sp(:lat,:lng,:radius)').params(lat = 1, lng = 1, radius=1).first()
+    result = db.session.query().from_statement('select number_of_fines from app_model_summary_sp(:lat,:lng,:radius)').params(lat = 1, lng = 1, radius=1).all()
     #result = db.engine.execute('SELECT app_model_summary_sp(?,?,?)', 1, 2, 3).fetchall()
     return 0
 
